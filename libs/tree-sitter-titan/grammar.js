a08445bd7c82d64b3e1c0727639e83dcbf1b6b44
Colin <colin.mtech@gmail.com>
2023-08-05 16:28:42 -0400

    feat(parser): add variable definitions

--- libs/tree-sitter-titan/grammar.js
+++ libs/tree-sitter-titan/grammar.js
@@ -1,7 +1,42 @@
 module.exports = grammar({
-  name: 'titan',
-
+  name: "titan",
   rules: {
-    source_file: $ => 'hello'
-  }
+    variable_definition: ($) =>
+      seq(
+        $._variable_start,
+        field("pattern", $.alpha_identifier),
+        optional(seq(":", field("type", $._type))),
+        "=",
+        field("value", $._non_null_literal)
+      ),
+
+    // TODO: Declare without assign
+    variable_declaration: ($) => seq(),
+
+    alpha_identifier: () =>
+      /[A-Za-z\$_][A-Z\$_a-z0-9]*(_[\-!#%&*+\/\\:<=>?@\u005e\u007c~]+)?/,
+
+    // TODO: Add annotations and modifiers and stuff
+    _variable_start: ($) => seq("let"),
+
+    // TODO: Add other types, maybe function/lambda
+    _type: ($) => choice($.literal_type),
+
+    // TODO: Add other types, string, float, etc
+    _non_null_literal: ($) => choice($.string, $.integer_literal),
+
+    // TODO: Replace with choice(simple string and multi line string)
+    string: ($) => $.alpha_identifier,
+
+    integer_literal: ($) =>
+      token(
+        seq(
+          optional(/[-]/), //negative
+          choice(/[\d](_?\d)*/, /0[xX][\da-fA-F](_?[\da-fA-F])*/), // digit or 0xdigits
+          optional(/[lL]/) // long
+        )
+      ),
+
+    literal_type: ($) => $._non_null_literal,
+  },
 });
