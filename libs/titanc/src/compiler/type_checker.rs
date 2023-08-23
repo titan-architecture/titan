dc9ebcf09f1c2cdfb3e8abddeb44cb90b3c30796
Hasan <45375125+hasanaburayyan@users.noreply.github.com>
2023-08-22 21:23:05 -0400

    feat(compiler): symbol tables and refactors (#22)
    Enhanced type checking and created symbol tables for use in type checker

--- libs/titanc/src/compiler/type_checker.rs
+++ libs/titanc/src/compiler/type_checker.rs
@@ -1,56 +1,74 @@
-use super::ast::{Scope, Statement, StatementKind, TypeKind};
+use super::{
+  ast::{Expression, ExpressionKind, Literal, Scope, Statement, StatementKind, TypeKind},
+  symbol_table::{Symbol, SymbolTable},
+};
 
-pub struct TypeChecker<> {
-
+pub struct TypeChecker {
+  symbol_table: SymbolTable,
 }
 
 impl TypeChecker {
   pub fn new() -> TypeChecker {
-    TypeChecker {}
-  }
-
-  pub fn type_check_scope(&self, scope: &Scope) {
-    for statement in &scope.statements {
-      self.type_check_statement(&statement);
+    TypeChecker {
+      symbol_table: SymbolTable::new(),
     }
   }
 
-  pub fn type_check_statement(&self, statement: &Statement) {
+  pub fn type_check_scope(&mut self, scope: &Scope) {
+    self.symbol_table.push_scope();
+
+    for statement in &scope.statements {
+      self.type_check_statement(&statement);
+    }
+
+    self.symbol_table.pop_scope();
+  }
+
+  pub fn type_of_expression(&mut self, expr: &Expression) -> TypeKind {
+    match &expr.kind {
+      ExpressionKind::Literal(lit) => match &lit {
+        Literal::Boolean(_) => TypeKind::Boolean,
+        Literal::Integer(_) => TypeKind::Integer,
+        Literal::String(_) => TypeKind::String,
+        _ => panic!("unexpected literal kind: {:?}", lit),
+      },
+      _ => panic!("unexpected expression kind: {:?}", &expr.kind),
+    }
+  }
+
+  pub fn assert_type_matches(&mut self, actual_type: &TypeKind, expected_type: &TypeKind) {
+    if !matches!(actual_type, expected_type) {
+      panic!("expected type: \"{:?}\" but got: \"{:?}\"", expected_type, actual_type);
+    }
+  }
+
+  pub fn type_check_statement(&mut self, statement: &Statement) {
     match &statement.kind {
-      StatementKind::Let { identifier, value, _type, .. } => {
-        if let None = &_type {
+      StatementKind::Let {
+        identifier,
+        value,
+        type_annotation,
+        ..
+      } => {
+        if let None = &type_annotation {
           panic!("Cannot infer types of variables yet");
         }
 
-        if let Some(typing) = _type {
-          match typing.kind {
-            TypeKind::String => {
-              if value.starts_with("\"") && value.ends_with("\"") {
-                return;
-              } else {
-                panic!("expected string literal for variable {:?} but got {:?}", identifier, value);
-              }
-            }
-            TypeKind::Integer => {
-              if value.parse::<i32>().is_ok() {
-                return;
-              } else {
-                panic!("expected integer literal for variable {:?} but got {:?}", identifier, value);
-              }
-            }
-            TypeKind::Boolean => {
-              if value == "true" || value == "false" {
-                return;
-              } else {
-                panic!("expected boolean literal for variable {:?} but got {:?}", identifier, value);
-              }
-            }
-            _ => {
-              panic!("cannot type check {:?} yet", typing.kind);
-            }
+        match self.symbol_table.add_symbol(Symbol {
+          name: identifier.clone(),
+          type_: type_annotation.as_ref().unwrap().kind,
+        }) {
+          Err(msg) => {
+            panic!("{}", msg);
           }
+          _ => {}
         }
+
+        // check type annotation against expression type
+        let expr_type = self.type_of_expression(value);
+        self.assert_type_matches(&expr_type, &type_annotation.as_ref().unwrap().kind)
       }
+      _ => {}
     }
   }
 }
